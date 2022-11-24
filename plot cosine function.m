fb00735b10bc8eebad07f7a9bb8ae1faad76f7df
Roman <113987210+romanjankovenko@users.noreply.github.com>
2022-11-24 15:04:29 +0100

    Update plot cosine function.m

--- plot cosine function.m
+++ plot cosine function.m
@@ -1,6 +1,6 @@
 % cosine wave with A = 10, f = 5 Hz and phase = pi/4 rad
 % plotted on time domain [0,0.5] with 1001 data points
 
-x = 0:0.5/1000:0.5;  % variable t, spanning from 0 to 0.5, with 1001 data points
-y = 10*cos(2*pi*5*x+pi/4);
-plot(x,y);
+t = 0:0.5/1000:0.5;  % variable t, spanning from 0 to 0.5, with 1001 data points
+x = 10*cos(2*pi*5*t+pi/4);
+plot(t,x);
